ab6b4133d7915418d8338f52118512fbea9580c9
kleo4004 <zkioz4004@gmail.com>
2021-08-24 23:07:18 +0300

    feat: add Cacher class

--- materials/workflow/cacher-task/script.js
+++ materials/workflow/cacher-task/script.js
@@ -1,1 +1,26 @@
-console.log(math.factorial(7));
+class Cacher {
+    constructor() {
+        this.cacheResult = Object.create(null);
+    }
+    withCache(fn) {
+        return number => {
+            const key = number.toString();
+            const value = this.cacheResult[key];
+            if (value) {
+                console.log('cache');
+                return value;
+            } else {
+                const result = fn(number);
+                this.cacheResult[key] = result;
+                console.log('new result');
+                return result;
+            }
+        }
+    }
+}
+
+const cacher = new Cacher();
+const cachedFactorial = cacher.withCache(math.factorial);
+console.log(cachedFactorial(7));
+console.log(cachedFactorial(7));
+console.log(cachedFactorial(5));
