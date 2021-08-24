861421dc7fa09440862fe772f0208aeabfe8b905
kleo4004 <zkioz4004@gmail.com>
2021-08-24 23:50:26 +0300

    feat: add some style for html

--- materials/workflow/cacher-task/script.js
+++ materials/workflow/cacher-task/script.js
@@ -10,7 +10,7 @@ class Cacher {
             const key = number.toString();
             const value = this.cacheResult[key];
             if (value) {
-                return `${value} from cache`;
+                return `${value} (from cache)`;
             } else {
                 const result = fn(number);
                 this.cacheResult[key] = result;
@@ -22,8 +22,13 @@ class Cacher {
 
 const showResult = () => {
     const inputValue = +input.value;
+    if (!inputValue) {
+        resultBlock.textContent = 'Enter number';
+        return false
+    }
     const resultText = cachedFactorial(inputValue);
     resultBlock.textContent = resultText;
+    input.value = '';
 }
 
 const cacher = new Cacher();
