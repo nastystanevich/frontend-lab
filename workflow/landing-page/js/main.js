5ae6903b4fd1e9684a888cadd1bc42822bdb4cc1
turbopushkin <alex.gorbelmu@gmail.com>
2023-10-23 00:08:00 +0300

    Added a marker to the landing page in the README.md

--- workflow/landing-page/js/main.js
+++ workflow/landing-page/js/main.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     popupDisplay();
-    showBurger()
+    showBurger();
+    // getData();
+    // createPagination();
 });
 
 function popupDisplay() {
@@ -30,4 +32,45 @@ function showBurger() {
         }
     }
 }
+
+async function getData() {
+    const dataApi = 'https://api.giphy.com/v1/gifs/search?api_key=hTfMt4wa3iLiabwXTQ1eTaKBiQ51Gr3J&q=cats&limit=27&offset=9';
+    const response = await fetch(dataApi)
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+async function createPagination() {
+    const postsData = await getData();
+    let currentPage = 1;
+    let cards = 9;
+
+    function initList(arrData, cardsPerPage, page) {
+        const collectionCards = document.querySelectorAll('.blogs__item');
+        const start = cardsPerPage * page;
+        const end = start + cardsPerPage;
+        const paginatedData = arrData.data.slice(start, end);
+
+        paginatedData.forEach((el, i) => {
+                const imageCard = collectionCards[i].querySelector('.image');
+                const titleCard = collectionCards[i].querySelector('.blogs__title');
+                const dateCard = collectionCards[i].querySelector('.blogs__date');
+                const linkCard = collectionCards[i].querySelector('a');
+
+                imageCard.src = el.images['original'].url;
+                titleCard.innerText = el.title;
+                dateCard.innerText = el['import_datetime'];
+                linkCard.href = el.url;
+        })
+    }
+
+    initList(postsData, cards, currentPage);
+    // function initPagination() {}
+}
+
+createPagination();
+
+
+
 
