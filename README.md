64b93f12f390f10c402ba42e785daa502fa64588
turbopushkin <alex.gorbelmu@gmail.com>
2023-10-23 21:28:36 +0300

    Fixed pagination.

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ So, if you're ready to work hard, go to [Getting Started](./materials/getting_st
 - [x] [Info](#frontend-lab)
 - [x] [Getting Started](./materials/getting_started.md)
 - [x] [Landing Page](./materials/landing_page.md)
-- [ ] [Landing Page - Working With Third-Party Services](./materials/landing_page_with_data.md)
+- [x] [Landing Page - Working With Third-Party Services](./materials/landing_page_with_data.md)
 - [ ] [Javascript - Custom Moment.js](./materials/js/custom_moment_js.md)
 - [ ] [Javascript - Cacher](./materials/js/cacher.md)
 - [ ] [Javascript - Tabs](./materials/js/tabs.md)
--- workflow/landing-page/js/main.js
+++ workflow/landing-page/js/main.js
@@ -1,101 +1,93 @@
 document.addEventListener('DOMContentLoaded', function() {
-    popupDisplay();
-    showBurger();
-    createPagination();
+    initPopupDisplay();
+    initBurger();
+    renderCards();
+    initPagination();
 });
 
-function popupDisplay() {
-    const buttonSearch = document.getElementById('button-search');
-    const popupSearch = document.getElementById('popup-search');
+function initPopupDisplay() {
+    const openPopupButton = document.getElementById('button-search');
+    const searchPopup = document.getElementById('popup-search');
 
-    buttonSearch.onclick = () => {
-        popupSearch.classList.add('active');
+    openPopupButton.onclick = () => {
+        searchPopup.classList.add('active');
 
-        const buttonClose = popupSearch.querySelector('.popup__close');
+        const closePopupButton = searchPopup.querySelector('.popup__close');
 
-        if(popupSearch.classList.contains('active')) {
-            buttonClose.onclick = () => {
-                popupSearch.classList.remove('active');
+        if(searchPopup.classList.contains('active')) {
+            closePopupButton.onclick = () => {
+                searchPopup.classList.remove('active');
             }
         }
     };
 }
 
-function showBurger() {
-    const burgerContent = document.getElementById('burger');
+function initBurger() {
+    const burgerContainer = document.getElementById('burger');
     const burgerButton = document.getElementById('menu-button');
 
-    if(burgerContent.classList.contains('close-burger')) {
-        burgerButton.onclick = () => {
-            burgerContent.classList.toggle('close-burger');
-        }
+    burgerButton.onclick = () => {
+        burgerContainer.classList.toggle('close-burger');
     }
+
 }
+
+let offset = 0;
 
 async function getData() {
-    const dataApi = 'https://api.giphy.com/v1/gifs/search?api_key=hTfMt4wa3iLiabwXTQ1eTaKBiQ51Gr3J&q=cats&limit=36&offset=9';
-    const response = await fetch(dataApi)
-    const data = await response.json();
-    return data;
+    try {
+        const giphyUrl = `https://api.giphy.com/v1/gifs/search?api_key=hTfMt4wa3iLiabwXTQ1eTaKBiQ51Gr3J&q=cats&limit=9&offset=${offset}`;
+        const response = await fetch(giphyUrl);
+        console.log(offset);
+
+        return response.json();
+    } catch (err) {
+        console.log(`Error during request: ${err.message}`)
+
+    }
 }
 
-async function createPagination() {
+async function renderCards() {
     const postsData = await getData();
-    const cards = 9;
-    let pages = postsData.data.length / cards;
-    let currentPage = 1;
+    if (!postsData) return;
+    const collectionCards = document.querySelectorAll('.blogs__item');
 
-    function initCards(arrData, cardsPerPage, page) {
-        const collectionCards = document.querySelectorAll('.blogs__item');
-        const start = (page - 1) * cardsPerPage;
-        const end = start + cardsPerPage;
-        const paginatedData = arrData.data.slice(start, end);
+    postsData.data.forEach((el, i) => {
+        const imageCard = collectionCards[i].querySelector('.image');
+        const titleCard = collectionCards[i].querySelector('.blogs__title');
+        const dateCard = collectionCards[i].querySelector('.blogs__date');
+        const linkCard = collectionCards[i].querySelector('a');
 
-        paginatedData.forEach((el, i) => {
-                const imageCard = collectionCards[i].querySelector('.image');
-                const titleCard = collectionCards[i].querySelector('.blogs__title');
-                const dateCard = collectionCards[i].querySelector('.blogs__date');
-                const linkCard = collectionCards[i].querySelector('a');
+        imageCard.src = el.images['original'].url;
+        titleCard.innerText = el.title;
+        dateCard.innerText = el['import_datetime'];
+        linkCard.href = el.url;
+    })
+}
 
-                imageCard.src = el.images['original'].url;
-                titleCard.innerText = el.title;
-                dateCard.innerText = el['import_datetime'];
-                linkCard.href = el.url;
-        })
+function initPagination() {
+    const prevArrow = document.querySelector('.prev');
+    const nextArrow = document.querySelector('.next');
 
-        initPagination();
+    nextArrow.onclick = () => {
+        offset += 9;
+        renderCards();
     }
 
-    initCards(postsData, cards, currentPage);
-
-    function initPagination() {
-        const prevArrow = document.querySelector('.prev');
-        const nextArrow = document.querySelector('.next');
-        //
-        // for (let i = 1; i <= pages; i++) {
-        //     let liBtn = document.createElement('li');
-        //     liBtn.className = 'arrows__item';
-        //     liBtn.innerText = `${i}`;
-        //     nextArrow.before(liBtn);
-        // }
-
-        nextArrow.onclick = () => {
-            currentPage++;
-            initCards(postsData, cards, currentPage);
+    prevArrow.onclick = () => {
+        if(offset === 0) {
+            return;
         }
-
-        prevArrow.onclick = () => {
-            currentPage--;
-            initCards(postsData, cards, currentPage);
-        }
-
-        if (currentPage > pages) {
-            currentPage = pages;
-        } else if(currentPage < 1) {
-            currentPage = 1;
-        }
+        offset -= 9;
+        renderCards();
     }
 }
+
+
+
+
+
+
 
 
 
