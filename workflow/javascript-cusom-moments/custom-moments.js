a4c4d136e599941bc180e1a058aaceefa9e7cd7a
turbopushkin <alex.gorbelmu@gmail.com>
2023-10-26 15:25:59 +0300

    Fix task one-moments and changed branch name

--- workflow/javascript-cusom-moments/custom-moments.js
+++ workflow/javascript-cusom-moments/custom-moments.js
@@ -1,40 +1,50 @@
-class OneMoment {
-    constructor(date) {
-        if(date instanceof Date) {
-            this.date = date;
-        } else {
-            this.date = new Date(date);
-        }
-    }
-
+class OneMoment extends Date{
     format(formDate) {
-        const year = this.date.getFullYear();
-        const month = this.date.getMonth();
-        const day = this.date.getDate();
+        const year = this.getFullYear();
+        let month = this.getMonth();
+        let day = this.getDate();
+
+        if(month < 10) month = '0' + month;
+        if(day < 10) day = '0' + day;
 
         return formDate.replace(/YYYY/i, year).replace(/MM/i, month).replace(/DD/i, day);
     }
 
-    formNow() {
-        const differenceDate = Math.abs(Date.now() - this.date);
-        const days = Math.ceil(differenceDate / (24 * 3600 * 1000));
-        const years = Math.ceil(differenceDate / (365 * 24 * 3600 * 1000));
+    fromNow() {
+        const differenceDate = Date.now() - this;
+        let days = Math.ceil(differenceDate / (24 * 3600 * 1000));
+        let years = Math.ceil(differenceDate / (365 * 24 * 3600 * 1000));
 
-        if(days <= 1) {
-            return `in ${days} day`;
-        } else if (days <= 364) {
-            return `in ${days} days`;
-        } else {
-            if(years === 1) {
-                return '1 year ago';
-            } else {
-                return `${years} years ago`;
+        if (differenceDate < 0) {
+            days = Math.abs(days);
+            years = Math.abs(years);
+
+            switch (true) {
+                case days <= 1:
+                    return `after ${days} day`;
+                case days <= 364:
+                    return `after ${days} days`;
+                case +years === 1:
+                    return 'in 1 year';
+                default:
+                    return `in ${years} years`;
             }
+        }
+
+        switch (true) {
+            case days <= 1:
+                return `in ${days} day`;
+            case days <= 364:
+                return `in ${days} days`;
+            case years === 1:
+                return '1 year ago';
+            default:
+                return `${years} years ago`;
         }
     }
 
     toDate() {
-        return new Date(this.date);
+        return this;
     }
 
     static parse(data, formatDate) {
@@ -45,17 +55,17 @@ class OneMoment {
         const month = +data.slice(formatDate.indexOf(formMonth), formatDate.indexOf(formMonth) + 2) - 1;
         const day = data.slice(formatDate.indexOf(formDay), formatDate.indexOf(formDay) + 2);
 
-        return new Date(year, month, day);
+        return new OneMoment(new Date(year, month, day));
     }
 }
 
-const someDate = new OneMoment(new Date(2023,10,10));
+const someDate = new OneMoment(new Date(2023,9,9));
 const anotherDate = OneMoment.parse('01202019', 'MMDDYYYY');
 
 
 let data = someDate.toDate();
 
 console.log('format(): ', someDate.format('YYYY/MM/DD'));
-console.log('formNow(): ', someDate.formNow());
+console.log('fromNow(): ', someDate.fromNow());
 console.log('is instance of Date: ', data instanceof Date);
-console.log('parse(): ', anotherDate instanceof Date, anotherDate);
+console.log('parse(): ', anotherDate instanceof OneMoment, anotherDate);
